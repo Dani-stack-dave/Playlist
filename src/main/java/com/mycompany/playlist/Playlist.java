d22e058a9c24815a48c4fec91d7ab49e7fd6a86b
SAM <SAM@DESKTOP-VSSKH6J>
2025-03-28 17:21:26 -0600

    Tarea de lo Playlist Para profesor Alfonso

---
+++ src/main/java/com/mycompany/playlist/Playlist.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.playlist;
+
+/**
+ *
+ * @author SAM
+ */
+public class Playlist {
+
+    public static void main(String[] args) {
+        Musicas P1=new Musicas("El paso del gigante", "Cumbias Mexicanas", "1987", 3);
+        
+        P1.ShowPlaylist();
+        
+    }
+}
